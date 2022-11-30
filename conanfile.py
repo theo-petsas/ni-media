04c51f0e388f3c710c9a58f57b650724a30e1764
Theo <15727624+theo-petsas@users.noreply.github.com>
2022-11-30 21:34:12 +0200

    update conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,40 +1,19 @@
 import os
 from conans import ConanFile, tools, CMake
 
-
-def get_version():
-    if os.environ.get("CONAN_PACKAGE_VERSION", None):
-        return f"{os.environ['CONAN_PACKAGE_VERSION']}"
-    return "0.0.0"
-
-
 class NiMediaConan(ConanFile):
-    name = "effects"
-    version = get_version()
+    name = "ni-media"
     topics = ("media", "audio decoding")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False, "boost:without_test": True}
-    requires = ["gtest/1.10.0", "boost/1.80.0"]
     generators = "cmake"
-    exports_sources = "*", "!.ccls-cache/*", "!build/*", "!compile_commands.json"
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["NIMEDIA_TESTS"] = "OFF"
+        cmake.definitions["NIMEDIA_ENABLE_FLAC_DECODING"] = "OFF"
+        cmake.definitions["NIMEDIA_ENABLE_OGG_DECODING"] = "OFF"
         cmake.configure()
         cmake.build()
-
-    def package(self):
-        self.copy("*.h", dst="include", src="include", keep_path=True)
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("*.h", src="audiostream/inc", dst="include/", keep_path=True)
-        self.copy("*.h", src="pcm/inc", dst="include/", keep_path=True)
-        self.copy("*.a", src="audiostream/", dst="lib/", keep_path=True)
-
 
     def package_info(self):
         self.cpp_info.libs = ["audiostream", "pcm"]
